67e02e35ae32f4e2be7b1ea2aafb35177ce92571
Besok <zhguchev@gmail.com>
2019-09-24 23:32:41 +0100

    add v

--- src/go/tinylang/graph/graph.go
+++ src/go/tinylang/graph/graph.go
@@ -28,23 +28,95 @@ func Relation(left *Vertex, right *Vertex) error {
 	err := left.add(right)
 	err = right.add(left)
 
-	log.Printf("relation between: [%d] - [%d]",left.Id,right.Id)
+	log.Printf("relation between: [%d] - [%d]", left.Id, right.Id)
 
 	return err
 }
 
+func (g *Graph) Relation(left *Vertex, right *Vertex) bool {
+	left, bL := g.FindById(left.Id)
+	right, bR := g.FindById(right.Id)
+
+	if !(bL && bR) {
+		return false
+	}
+
+	err := Relation(left, right)
+	if err != nil {
+		log.Println("error while adding to gragh: ", err)
+		return false
+	}
+	return true
+}
+
+func (g *Graph) FindById(id int) (*Vertex, bool) {
+	vertex := g.root
+
+	if vertex.Id == id{
+		return vertex,true
+	}
+	queue := NewQueue()
+
+	for
 
 
+	return nil, false
+}
+
+func (q *Queue) PushV(vm *Vertex, mark bool) {
+	vmark := VertexMark{Marked: mark, Vertex: *vm}
+	q.Push(&vmark)
+}
+
+func (q *Queue) Push(vm *VertexMark) {
+	*q = append(*q, *vm)
+}
+func (q *Queue) Len() int {
+	return len(*q)
+}
+func (q *Queue) Pop() *VertexMark {
+	ln := len(*q)
+	last := (*q)[ln-1]
+
+	*q = (*q)[:ln-1]
+
+	return &last
+}
+
+
+type Queue []VertexMark
+
+
+func NewQueue() *Queue {
+	queues := make(Queue, 0)
+	return &queues
+}
+
+type VertexMark struct {
+	Vertex Vertex
+	Marked bool
+}
+
+func (v *Vertex) checkId(id int) bool {
+	for _, el := range v.Neighbours {
+		if el.Id == id {
+			return true
+		}
+	}
+	return false
+}
 func (v *Vertex) add(n *Vertex) error {
+	if v.checkId(n.Id) {
+		return nil
+	}
+
 	v.Neighbours = append(v.Neighbours, n)
 	return nil
 }
 
-
 type Error struct {
 	msg string
 }
-
 
 func (e Error) Error() string {
 	return e.msg
--- src/go/tinylang/graph/graph_test.go
+++ src/go/tinylang/graph/graph_test.go
@@ -22,6 +22,54 @@ func Test_commonGraph(t *testing.T){
 
 }
 
+func Test_MarkQueue(t *testing.T){
+	v1 := NewVertex(1, StringValue("Test1"))
+	v2 := NewVertex(2, StringValue("Test2"))
+	v3 := NewVertex(3, StringValue("Test3"))
+
+	queue := NewQueue()
+	queue.PushV(v1,false)
+	queue.PushV(v2,false)
+	queue.PushV(v3,false)
+
+	if queue.Len() < 3{
+		t.Fatalf("error - needs at least 3")
+	}
+
+	_ = queue.Pop()
+	if queue.Len() > 2{
+		t.Fatalf("error - needs at least 3")
+	}
+
+}
+func Test_GraphRelation(t *testing.T){
+	v1 := NewVertex(1, StringValue("Test1"))
+	v2 := NewVertex(2, StringValue("Test2"))
+	v3 := NewVertex(3, StringValue("Test3"))
+	v4 := NewVertex(4, StringValue("Test4"))
+	v5 := NewVertex(5, StringValue("Test5"))
+
+	err := Relation(v1, v2)
+	err = Relation(v2, v3)
+	err = Relation(v3, v4)
+	err = Relation(v4, v5)
+
+	if err != nil {
+		t.Fatalf("bad dec")
+	}
+
+	graph := NewGraph(v1)
+	relationFalse := graph.Relation(v1, v2)
+	relationTrue := graph.Relation(v1, v3)
+
+	if relationFalse {
+		t.Fatalf("bad dec")
+	}
+	if !relationTrue {
+		t.Fatalf("bad dec")
+	}
+}
+
 type StringValue string
 
 func (v StringValue) Get() interface{} {
